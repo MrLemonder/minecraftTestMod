fec03226168243a592704cb6de6cb86bc7e14c7b
Kirill Gorodilov <kirillgorodil95@gmail.com>
2024-03-16 00:29:37 +0100

    Added creative beer tab

---
+++ src/main/java/net/mrlemonder/beersushicraft/item/ModCreativeModeTab.java
@@ -0,0 +1,31 @@
+package net.mrlemonder.beersushicraft.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.mrlemonder.beersushicraft.BeerSushiCraft;
+
+public class ModCreativeModeTab {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BeerSushiCraft.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> BEER_SUSHI_TAB = CREATIVE_MODE_TABS.register("beer_sushi_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ModItems.BEER.get()))
+                    .title(Component.translatable("creativetab.beer_sushi_tab"))
+                    .displayItems((itemDisplayParameters, output) ->
+                    {
+                        output.accept(ModItems.BEER.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus)
+    {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
